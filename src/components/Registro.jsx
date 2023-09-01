26e365fc7cad011d97deeeee2f1454800f9bc398
Tansuani Waring <tansuanigomezw@gmail.com>
2023-09-01 15:43:28 -0400

    Se agregan botones de RRSS

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,8 +1,15 @@
-
+import SocialButton from "./SocialButton"
 
 const Registro = () => {
   return (
-    <div>Registro</div>
+    <>
+      <div>Registro</div>
+      <SocialButton 
+        facebook="fa-brands fa-facebook fa-fade"
+        github="fa-brands fa-github fa-fade"
+        linkedin="fa-brands fa-linkedin-in fa-fade"
+      />
+    </>
   )
 }
 
